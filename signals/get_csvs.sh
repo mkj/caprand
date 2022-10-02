cb123360b3e4b533a9c55dc78b99706cb8addc4c
Matt Johnston <matt@ucc.asn.au>
2022-10-02 13:16:09 +0800

    fix get_csvs

---
+++ signals/get_csvs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+fn="$1"
+
+function ex() {
+	dir="$1"
+	fn="$2"
+
+	(echo t_$dir,imm_$dir,drive_$dir; grep t_$dir "$fn" | head -n 100000 | cut -d ' ' -f 5,6,9|sed 's/ /,/g' ) > "$fn.$dir"
+}
+
+ex down "$fn"
+ex up "$fn"
+
+
