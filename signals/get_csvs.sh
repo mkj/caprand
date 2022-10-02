6864f9ac7d999501c98772eb56cd943963e3dcd1
Matt Johnston <matt@ucc.asn.au>
2022-10-02 13:48:33 +0800

    Add more data

--- signals/get_csvs.sh
+++ signals/get_csvs.sh
@@ -6,7 +6,7 @@ function ex() {
 	dir="$1"
 	fn="$2"
 
-	(echo t_$dir,imm_$dir,drive_$dir; grep t_$dir "$fn" | head -n 100000 | cut -d ' ' -f 5,6,9|sed 's/ /,/g' ) > "$fn.$dir"
+	(echo t_$dir,imm_$dir,drive_$dir; grep t_$dir "$fn" | tail -n +200 | head -n 100000 | cut -d ' ' -f 5,6,9|sed 's/ /,/g' ) > "$fn.$dir"
 }
 
 ex down "$fn"
